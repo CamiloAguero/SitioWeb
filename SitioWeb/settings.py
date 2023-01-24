2cf0eca05f8f78ad25e710404bb8e3e59a825a77
Camil <camilo.iac2001@gmail.com>
2023-01-24 14:36:47 -0300

    1.3.2

--- SitioWeb/settings.py
+++ SitioWeb/settings.py
@@ -58,7 +58,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    #'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -173,4 +173,4 @@ DATE_INPUT_FORMATS = ('%d-%m-%Y')
 
 DATETIME_INPUT_FORMATS = ('%H:%M')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
