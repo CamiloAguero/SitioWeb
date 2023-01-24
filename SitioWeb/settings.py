25d1bbf8c9b3f49b1f3de81d8aa813b14ad254e2
Camil <camilo.iac2001@gmail.com>
2023-01-24 15:13:56 -0300

    1.3.9

--- SitioWeb/settings.py
+++ SitioWeb/settings.py
@@ -54,6 +54,8 @@ INSTALLED_APPS = [
     'crispy_forms',
     'ver_reservas',
     'fichas',
+    "whitenoise.runserver_nostatic",
+    "django.contrib.staticfiles",
 ]
 
 MIDDLEWARE = [
@@ -141,16 +143,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-#STATIC_URL = 'static/'
-STATIC_HOST = os.environ.get("DJANGO_STATIC_HOST", "")
-STATIC_URL = STATIC_HOST + "/static/"
+STATIC_URL = 'static/'
 
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR,'staticfiles')
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+MEDIA_ROOT = os.path.join(BASE_DIR,'static')
 
 
 
