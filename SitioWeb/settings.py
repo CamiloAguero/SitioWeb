9e4b18baf4c5949ec35f79ee39413421ef9068db
Camil <camilo.iac2001@gmail.com>
2023-01-25 11:02:25 -0300

    1.6.3

--- SitioWeb/settings.py
+++ SitioWeb/settings.py
@@ -77,7 +77,7 @@ ROOT_URLCONF = 'SitioWeb.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
