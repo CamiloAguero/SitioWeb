b65de88b3c403e700140c323bbdf447fa11e13b5
Camil <camilo.iac2001@gmail.com>
2023-01-25 12:13:13 -0300

    1.6.6

--- SitioWeb/settings.py
+++ SitioWeb/settings.py
@@ -43,7 +43,7 @@ if RENDER_EXTERNAL_HOSTNAME:
 # Application definition
 
 INSTALLED_APPS = [
-    'SitioWebApp.apps.RenderConfig',
+    'SitioWeb.apps.RenderConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
